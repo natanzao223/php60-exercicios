2ced1bf05f92ea2bf15695b594c006e472626471
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:48:48 -0300

    Update exercicios1.php

--- exercicios1.php
+++ exercicios1.php
@@ -1,1 +1,22 @@
 
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio01 Escreva um programa que exiba na tela do computador a mensagem: 'Aprendendo PHP!!!'.
+
+echo "Aprendendo PHP!!!";
+
+//O programa utiliza o comando echo para exibir a mensagem na tela.
+
+?>
