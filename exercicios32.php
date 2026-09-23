de03790b210f8f19dc302a557668d5d739d93dab
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 16:00:41 -0300

    Add program to evaluate number sign
    This program reads a real number and determines if it is positive, negative, or zero.

---
+++ exercicios32.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ADS2A
+Componentes:
+26013158-2-Jackson Rodrigues
+26013927-2-Natan da Silva
+26014123-2-Gustavo Tristão
+26014469-2-Rafael Baliski
+Data: 23 de Setembro de 2026
+*/
+//exercicio32 Escreva um programa que leia um número real e informe se ele é positivo, negativo ou nulo.
+$numero = -5;
+
+if ($numero > 0) {
+    echo "Positivo";
+} elseif ($numero < 0) {
+    echo "Negativo";
+} else {
+    echo "Nulo";
+}
+
+//O programa compara o número com zero, se for maior que 0 é positivo se for menor, é negativo e se for igual a 0, é nulo
+
+?>
