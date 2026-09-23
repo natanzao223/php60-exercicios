f608ae882c4981b8243144b9367ccd08efb2d139
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:49:09 -0300

    Add PHP script to display integer variable value
    This PHP script declares an integer variable, assigns it the value 10, and displays that value.

---
+++ exercicio2.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio02 Declare uma variável inteira, atribua o valor 10 e exiba seu valor na tela.
+
+$x = 10;
+
+echo "Valor de x = $x";
+
+//O programa armazena o valor 10 na variável x e utiliza o echo para exibir esse valor.
+
+?>
