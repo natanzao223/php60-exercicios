34193493e6872a7cbfb906d3d655de6d9a0eeb24
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:50:27 -0300

    Add script to calculate product of two numbers
    This PHP script calculates and displays the product of 28 and 43.

---
+++ exercicio6.php
@@ -0,0 +1,26 @@
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio06 Calcule e exiba o produto entre os números 28 e 43.
+
+$numero1 = 28;
+$numero2 = 43;
+
+$produto = $numero1 * $numero2;
+
+echo "Produto = $produto";
+
+//O programa multiplica os números 28 e 43 e armazena o resultado na variável produto.
+
+?>
