ea22417067ea2446e83effb456b5e5952c552b20
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:49:23 -0300

    Create exercicio3.php to demonstrate variable increment
    This PHP script declares an integer variable with a value of 10 and displays its value incremented by one without modifying the original variable.

---
+++ exercicio3.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio03 Declare uma variável inteira com valor 10 e exiba seu valor acrescido em uma unidade sem alterar a variável original.
+
+$x = 10;
+
+echo "Resultado = " . ($x + 1);
+
+//O programa soma 1 ao valor de x somente na exibição, mantendo o valor original da variável igual a 10.
+
+?>
