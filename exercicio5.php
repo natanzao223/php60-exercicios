f4a4fa92cffb3ca90ccaecef5d3b4cf1452ead42
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:49:57 -0300

    Add exercicio5.php for user greeting
    Create a PHP script to greet the user by name.

---
+++ exercicio5.php
@@ -0,0 +1,24 @@
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio05 Obtenha o nome do usuário via teclado e exiba uma mensagem de boas-vindas.
+
+echo "Digite seu nome: ";
+$nome = trim(fgets(STDIN));
+
+echo "Olá $nome!";
+
+//O programa recebe o nome digitado pelo usuário e utiliza o echo para mostrar uma mensagem de boas-vindas.
+
+?>
