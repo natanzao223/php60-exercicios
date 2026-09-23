b174836fd618b8c1ba41243d13c618e973c3f7e8
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 17:50:48 -0300

    Add PHP script to calculate arithmetic mean
    This script calculates and displays the arithmetic mean of three numbers.

---
+++ exercicio7.php
@@ -0,0 +1,27 @@
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
+
+//exercicio07 Calcule e exiba a média aritmética entre os números 8, 9 e 7.
+
+$numero1 = 8;
+$numero2 = 9;
+$numero3 = 7;
+
+$media = ($numero1 + $numero2 + $numero3) / 3;
+
+echo "Média = $media";
+
+//O programa soma os três números e divide o resultado por 3 para calcular a média aritmética.
+
+?>
