82a852e34e8264afc2ffd799d24ee678b9327a82
natanzao223 <natanrhyannebu@gmail.com>
2026-09-23 15:57:51 -0300

    Update exercicios31.php

--- exercicios31.php
+++ exercicios31.php
@@ -1,25 +1,27 @@
-<?php
-/*
-Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ADS2A
-Componentes:
-26013158-2-Jackson Rodrigues
-26013927-2-Natan da Silva
-26014123-2-Gustavo Tristão
-26014469-2-Rafael Baliski
-Data: 17 de Setembro de 2026
-*/
-// exercicio 31 Escreva um programa que leia um número inteiro e exiba se ele é PAR ou ÍMPAR
+<?php 
+/* 
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas 
+Disciplina: Linguagem e Técnicas de Programação 
+Professor: Flores 
+Turma: ADS2A 
+Componentes: 
+26013158-2-Jackson Rodrigues 
+26013927-2-Natan da Silva 
+26014123-2-Gustavo Tristão 
+26014469-2-Rafael Baliski 
+Data: 23 de Setembro de 2026 
+*/ 
 
-$numero = (int) readline("Digite um número inteiro: ");
+//exercicio31 Escreva um programa que leia um número inteiro e exiba se ele é PAR ou ÍMPAR.
+
+$numero = 5;
 
 if ($numero % 2 == 0) {
-    echo "O número $numero é par";
+    echo "O número é PAR";
 } else {
-    echo "O número $numero é ímpar";
+    echo "O número é ÍMPAR";
 }
-//O programa lê um número e usa o operador % para verificar o resto da divisão por 2. Se o resto for 0, o número é par caso contrário, é ímpar
+
+//O programa divide o número por 2 e verifica o resto. Se o resto for 0 é par, caso contrário é ímpar.
 
 ?>
